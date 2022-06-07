87a4cd97329088c2f4400722219abba70f144025
Benjamin Hesse <100107950+behesse@users.noreply.github.com>
2022-06-07 19:55:47 +0200

    Fix version pin

--- opt/scripts/update.sh
+++ opt/scripts/update.sh
@@ -10,7 +10,7 @@ if [ -n "${rv}" ] && [ "${cv}" != "${rv}" ]; then
     echo "Stopping server..."
     [[ $(supervisorctl -c /opt/config/supervisord.conf status eco) =~ "RUNNING" ]] && supervisorctl -c /opt/config/supervisord.conf stop eco
     echo "Updating..."
-    /home/steam/steamcmd/steamcmd.sh +force_install_dir /opt/eco +login anonymous +app_update 739590 -beta 9.5.1 validate +quit
+    /home/steam/steamcmd/steamcmd.sh +force_install_dir /opt/eco +login anonymous +app_update 739590 validate +quit
     echo "Updated to build `grep buildid "/opt/eco/steamapps/appmanifest_739590.acf" | tr '"' ' ' | awk '{print $2}'`"
 fi
 
